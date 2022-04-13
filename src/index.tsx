b5df3b9680d01f24b8df92faf93b974b7b08227a
Lee Min Wook <dnr14@naver.com>
2022-04-13 21:58:42 +0900

    chore : 프로젝트 설정 1. theme 파일 설정 2. 테스트코드 폴더 설정

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter, Link, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { ThemeProvider } from 'styled-components';
+import { theme } from 'styles/theme';
+import GlobalStyle from 'styles/global';
 
 ReactDOM.render(
   <React.StrictMode>
-    <BrowserRouter>
-      <Link to={'/app'}>app 이동</Link>
-      <Route path={'/app'}>
+    <GlobalStyle />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
         <App />
-      </Route>
-    </BrowserRouter>
+      </BrowserRouter>
+    </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root'),
 );
